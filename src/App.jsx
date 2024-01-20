f74446f0e4fd32c2c61215bf45f397f65d70faad
danielbelle <henrique.danielb@gmail.com>
2024-01-20 00:53:13 -0300

    v1.0 - iniciando o projeto com o css ok

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Welcome from './components/Welcome'
+import Question from './components/Question'
+import { useContext, useEffect } from 'react'
+import { QuizContext } from './context/quiz'
+
+function App() {
+
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" })
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Quiz do Dev</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import { QuizContext } from '../context/quiz'
+import Quiz from '../img/quiz.svg'
+import './Welcome.css'
+
+function Welcome() {
+
+  const [QuizState, dispatch] = useContext(QuizContext);
+
+  return (
+
+    <div id='welcome' >
+      <h2>Seja Bem-Vindo</h2>
+      <p>Clique no botão para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
+      <img src={Quiz} alt='início do quiz'></img>
+    </div>
+  )
+}
+
+export default Welcome
